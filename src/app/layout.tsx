ca72c2f89a76acd9ec940884da200ac6a5a1daac
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-07 21:42:47 +0530

    simple navbar created with favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
+import logo from "./favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Thahawuru | Sri Lanka's Integrated Digital Identity Verification System",
-  description: "Thahawuru is a digital identity verification system that allows you to verify your identity via a digital wallet application.",
+  title:
+    "Thahawuru | Sri Lanka's Integrated Digital Identity Verification System",
+  description:
+    "Thahawuru is a digital identity verification system that allows you to verify your identity via a digital wallet application.",
 };
 
 type RootLayoutProps = {
@@ -17,7 +20,9 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>{/* Add any additional head elements here if needed */}</head>
+      <head>
+        <link rel="icon" href={logo.src} />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,45 @@
+"use client";
+
+import logo from "./Thahawuru.png";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
+  const items = [
+    { title: "Home", link: "/" },
+    { title: "About", link: "/about" },
+    { title: "Contact", link: "/contact" },
+    { title: "API", link: "/api" },
+    { title: "Login", link: "/login" },
+  ];
+
   return (
     <>
-      <div className="bg-destructive w-full h-[60px]">
-        {/* Add your navbar content here */}
+      <div className="fixed bg-whiteTwo border border-black-900 w-full h-[60px]">
+        <div className="container mx-auto flex justify-between items-center h-full">
+          <div className="text-secondary font-bold">
+            <Link href="/" passHref>
+              <Image
+                className="w-[50px] object-cover rounded-full transition transform hover:scale-110 duration-500 ease-in-out cursor-pointer"
+                src={logo}
+                alt="Logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+          </div>
+          <div className="flex items-center space-x-4">
+            {items.map((item) => (
+              <Link
+                key={item.title}
+                href={item.link}
+                className="text-secondary"
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
