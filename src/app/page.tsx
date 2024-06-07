90cdb196d3f15cd8e5cd5f7ae2ee2211409487c0
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-07 20:30:31 +0530

    Tailwind color themes custormized and now can be continued

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-primary">
+        <div className="container">
+          <div className="flex justify-between items-center py-4 w-full h-screen">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
