a382f6cbbd92c911ddbe079a877e133a18e8517c
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-08 21:20:17 +0530

    Frontend Style modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,8 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col h-auto w-full justify-center items-center bg-gradient-to-br from-white to-primary">
-        <div className="flex flex-row justify-between items-center w-full h-screen border">
-          <div className="flex flex-col w-[70%] bg-gradient-to-br from-white to-primary h-full text-black justify-center items-center p-20">
+        <div className="flex flex-row justify-between items-center w-full h-screen bg-gradient-to-br from-secondary to-secondaryTwo">
+          <div className="flex flex-col w-[70%] h-full text-white justify-center items-center p-20">
             <div className="w-[90%]">
               <motion.main
                 initial={{ opacity: 0, y: 50 }}
@@ -54,7 +54,7 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="w-full h-auto flex flex-col"
               >
-                <h1 className="text-6xl text-secondary">
+                <h1 className="text-6xl text-white">
                   <b>තහවුරු</b>
                 </h1>
               </motion.main>
@@ -65,7 +65,7 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="w-full h-auto flex flex-col"
               >
-                <h2 className="text-2xl mt-4 text-secondary">
+                <h2 className="text-2xl mt-4 text-white">
                   <b>
                     Sri Lanka`s Integrated Digital Identity Verification System
                   </b>
@@ -78,7 +78,7 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="w-full h-auto flex flex-col"
               >
-                <p className="text-lg mt-4 text-secondary text-sm">
+                <p className="text-lg mt-4 text-white text-sm">
                   Thahawuru is a digital identity verification system that
                   allows you to verify your identity with a single QR scan. It
                   is a secure and reliable digital wallet which keep your
@@ -94,7 +94,7 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="w-full h-auto flex flex-col"
               >
-                <p className="text-lg mt-4 text-secondary text-sm">
+                <p className="text-lg mt-4 text-white text-sm">
                   <b>
                     This will bring a new era of digital verification system to
                     Sri Lanka.
@@ -108,39 +108,41 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="w-[140px] h-auto flex flex-col"
               >
-                <button className="bg-secondary hover:bg-secondaryTwo text-white mt-4 p-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+                <button className="bg-white text-secondary mt-4 p-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
                   Get Started
                 </button>
               </motion.main>
             </div>
           </div>
-          <div className="flex flex-col w-[30%] bg-primary h-full justify-center items-left">
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              variants={zoomInVariants}
-              className="w-[70%] h-3/4 border rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105 flex flex-col justify-center items-center cursor-pointer"
-            >
-              <div className="w-[80%]">
-                <Image src={logo} alt="Profile Picture"></Image>
-              </div>
-              <div className="w-[70%] flex flex-col justify-center items-center">
-                <h2 className="text-2xl text-secondary">
-                  <b>Scan Me</b>
-                </h2>
-                <h1 className="text-1xl text-secondary">
-                  Download your Mobile App
-                </h1>
-              </div>
-              <div className="w-[50%] mt-10">
-                <Image src={testQR} alt="Test QR"></Image>
-              </div>
-            </motion.div>
+          <div className="flex flex-col w-[30%] h-full justify-center items-left">
+            <div className="transition duration-300 ease-in-out transform hover:scale-105 w-[70%] h-3/4 mt-16">
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                variants={zoomInVariants}
+                className="h-full w-full rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 flex flex-col justify-center items-center cursor-pointer bg-white"
+              >
+                <div className="w-[80%]">
+                  <Image src={logo} alt="Profile Picture"></Image>
+                </div>
+                <div className="w-[70%] flex flex-col justify-center items-center">
+                  <h2 className="text-2xl text-secondary">
+                    <b>Scan Me</b>
+                  </h2>
+                  <h1 className="text-1xl text-secondary">
+                    Download your Mobile App
+                  </h1>
+                </div>
+                <div className="w-[50%] mt-10">
+                  <Image src={testQR} alt="Test QR"></Image>
+                </div>
+              </motion.div>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between items-center w-4/5 h-screen">
-          <h1 className="text-4xl text-secondary text-center">
+        <div className="flex flex-col justify-between items-center w-4/5 h-screen mb-28">
+          <h1 className="text-4xl text-secondary text-center mt-20">
             <b>Features we offer</b>
           </h1>
           <h2 className="text-2xl text-secondary text-center">
@@ -167,11 +169,11 @@ export default function Home() {
             ))}
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center w-full h-auto pt-20">
-          <h1 className="text-4xl text-secondary text-center">
+        <div className="flex flex-col justify-center items-center w-full h-auto pt-20 mt-10 pb-10 bg-gradient-to-br from-secondary to-secondaryTwo">
+          <h1 className="text-4xl text-white text-center">
             <b>Have a Question?</b>
           </h1>
-          <h2 className="text-2xl text-secondary text-center">
+          <h2 className="text-2xl text-white text-center">
             Find your questions and solve it from here
           </h2>
           <div className="w-3/4 h-auto">
