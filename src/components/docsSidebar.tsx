b8b29bc2ab0d0aeac33300e332d455eef6ae6604
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-16 16:24:33 +0530

    Docs SideBar Modified

--- src/components/docsSidebar.tsx
+++ src/components/docsSidebar.tsx
@@ -19,19 +19,35 @@ import logo from "../../public/thahawuruText.png";
 export default function DocsSideBar() {
   const [isOpen, setIsOpen] = useState(false);
   const items = [
-    { title: "Dashboard", link: "/docs/dashboard", icon: <BiHomeAlt /> },
-    { title: "Overview", link: "/docs/overview", icon: <BiFile /> },
+    {
+      title: "Dashboard",
+      link: "/docs/dashboard",
+      icon: <BiHomeAlt className="text-secondary" />,
+    },
+    {
+      title: "Overview",
+      link: "/docs/overview",
+      icon: <BiFile className="text-secondary" />,
+    },
     {
       title: "Getting Started",
       link: "/docs/getting-started",
-      icon: <BiRocket />,
+      icon: <BiRocket className="text-secondary" />,
     },
-    { title: "API", link: "/docs/api", icon: <BiBook /> },
-    { title: "TestAPI", link: "/docs/testAPI", icon: <BiTestTube /> },
+    {
+      title: "API",
+      link: "/docs/api",
+      icon: <BiBook className="text-secondary" />,
+    },
+    {
+      title: "TestAPI",
+      link: "/docs/testAPI",
+      icon: <BiTestTube className="text-secondary" />,
+    },
     {
       title: "Agreement & Buy",
       link: "/docs/agreement",
-      icon: <BiShoppingBag />,
+      icon: <BiShoppingBag className="text-secondary" />,
     },
   ];
 
@@ -40,11 +56,11 @@ export default function DocsSideBar() {
   };
 
   return (
-    <div className="relative min-h-screen flex">
+    <div className="relative min-h-screen flex shadow-lg">
       <div
         className={`transition-all duration-300 ${
           isOpen ? "w-64" : "w-16"
-        } bg-gradient-to-br from-secondary to-secondaryTwo dark:bg-gray-800 h-full overflow-hidden`}
+        } bg-gradient-to-br from-primary to-white dark:bg-gray-800 h-full overflow-hidden`}
       >
         <div className="flex items-center justify-center h-16">
           <Image
@@ -62,9 +78,9 @@ export default function DocsSideBar() {
             onClick={toggleSidebar}
           >
             {isOpen ? (
-              <BiChevronLeft className="mr-2 w-5 h-5 rounded-lg text-white border border-white" />
+              <BiChevronLeft className="mr-2 w-5 h-5 rounded-lg text-secondary border border-secondary" />
             ) : (
-              <BiChevronRight className="mr-2 w-5 h-5 rounded-lg  text-white border border-white" />
+              <BiChevronRight className="mr-2 w-5 h-5 rounded-lg text-secondary border border-secondary" />
             )}
           </button>
         </div>
@@ -74,12 +90,16 @@ export default function DocsSideBar() {
             {items.map((item, index) => (
               <li key={index}>
                 <Link href={item.link}>
-                  <div className="flex items-center p-2 text-white-900 rounded-lg dark:text-white hover:bg-white-100 dark:hover:bg-white-700 group">
-                    <div className="flex-shrink-0 w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-white-900 dark:group-hover:text-white">
+                  <div className="flex items-center p-2 text-secondary-900 rounded-lg dark:text-secondary hover:bg-secondary-100 dark:hover:bg-secondary-700 group">
+                    <div className="flex-shrink-0 w-5 h-5 text-secondary-500 transition duration-75 dark:text-secondary-400 group-hover:text-secondary-900 dark:group-hover:text-secondary">
                       {item.icon}
                     </div>
-                    <span className={`ml-3 ${isOpen ? "inline" : "hidden"}`}>
-                      {item.title}
+                    <span
+                      className={`ml-3 ${
+                        isOpen ? "inline text-secondary text-sm hover:text-secondaryTwo" : "hidden"
+                      }`}
+                    >
+                      <b>{item.title}</b>
                     </span>
                   </div>
                 </Link>
