6eb0b6de3f2569f20ff4673f2581578f026a628b
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-06-17 00:43:53 +0530

    Docs removed and minor fixes in docs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const items = [
     { title: "Home", link: "/" },
-    { title: "docs", link: "/docs/dashboard" },
+    { title: "docs", link: "https://thahawuru-docs.vercel.app/", external: true }, // Added external: true for external links
     { title: "Login", link: "/login" },
   ];
 
@@ -29,7 +29,10 @@ export default function Navbar() {
               <Link
                 key={item.title}
                 href={item.link}
+                passHref={!item.external} // Use passHref only if it's not an external link
                 className="text-secondary hover:text-secondaryTwo transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer text-1xl"
+                target={item.external ? "_blank" : undefined} // Set target="_blank" for external links
+                rel={item.external ? "noopener noreferrer" : undefined} // Add rel="noopener noreferrer" for security reasons on external links
               >
                 <div className="bg-secondary text-white p-1">
                   &nbsp;<b>{item.title}</b>&nbsp;
@@ -80,7 +83,10 @@ export default function Navbar() {
                 <Link
                   key={item.title}
                   href={item.link}
+                  passHref={!item.external} // Use passHref only if it's not an external link
                   className="text-secondary hover:text-secondaryTwo transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer text-1xl"
+                  target={item.external ? "_blank" : undefined} // Set target="_blank" for external links
+                  rel={item.external ? "noopener noreferrer" : undefined} // Add rel="noopener noreferrer" for security reasons on external links
                 >
                   <div className="bg-secondary text-white p-1">
                     &nbsp;<b>{item.title}</b>&nbsp;
