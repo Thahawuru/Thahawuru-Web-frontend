28081c5b6875f1c073034f18b6324cbb37d59e14
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-07-10 23:14:15 +0530

    Admin Dashboard Done

---
+++ src/app/admin/dashboard/charts/LineColumnChart.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useRef } from "react";
+import ApexCharts from "apexcharts";
+
+const LineColumnChart = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const chartOptions = {
+      series: [
+        {
+          type: "column",
+          data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+        },
+        {
+          type: "area",
+          data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+        },
+        {
+          type: "line",
+          data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        stacked: false,
+        toolbar: {
+          show: false,
+        },
+      },
+      stroke: {
+        width: [0, 2, 5],
+        curve: "smooth",
+        colors: ["#023e8a", "#adb5bd"], // Added stroke colors
+      },
+      fill: {
+        opacity: [0.85, 0.25, 1],
+        gradient: {
+          inverseColors: false,
+          shade: "light",
+          type: "vertical",
+          opacityFrom: 0.85,
+          opacityTo: 0.55,
+          stops: [0, 100, 100, 100],
+        },
+        colors: ["#023e8a", "#adb5bd"], // Added fill colors
+      },
+      labels: [
+        "01/01/2003",
+        "02/01/2003",
+        "03/01/2003",
+        "04/01/2003",
+        "05/01/2003",
+        "06/01/2003",
+        "07/01/2003",
+        "08/01/2003",
+        "09/01/2003",
+        "10/01/2003",
+        "11/01/2003",
+      ],
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        title: {
+          text: "Points",
+        },
+        min: 0,
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+          formatter: function (y: any) {
+            if (typeof y !== "undefined") {
+              return y.toFixed(0) + " points";
+            }
+            return y;
+          },
+        },
+        style: {
+          fontSize: "12px",
+          colors: ["#023e8a"],
+        },
+      },
+      colors: ["#023e8a", "#adb5bd"],
+    };
+
+    if (chartRef.current) {
+      const chart = new ApexCharts(chartRef.current, chartOptions);
+      chart.render();
+
+      return () => {
+        chart.destroy();
+      };
+    }
+  }, []);
+
+  return <div ref={chartRef} id="chart" />;
+};
+
+export default LineColumnChart;
