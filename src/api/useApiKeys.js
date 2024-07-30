13efac0f5e7f78a45d4be98f4cc4236fe253a4bc
Dinirubanuka <105926294+Dinirubanuka@users.noreply.github.com>
2024-07-30 08:08:39 +0530

    admin accept reject api reqeust

--- src/api/useApiKeys.js
+++ src/api/useApiKeys.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import apiClient from "./apiClient";
+import Toast from "@/components/utils/toaster";
 
 export const useApiKeys = () => {
   const createApiKey = async (data) => {
@@ -24,8 +25,41 @@ export const useApiKeys = () => {
     }
   };
 
+  const getAllApiRequests = async () => {
+    try{
+      const response = await apiClient.get(`/admin/Api/all`);
+      return response;
+    }catch (error){
+      throw new Error(error.response.data.error.message);
+    }
+  };
+
+  const acceptApiRequests = async (apiId) => {
+    try{
+      const response = await apiClient.get(`/admin/Api/accept/${apiId}`);
+      return response;
+
+    }catch (error){
+      throw new Error(error.response.data.error.message);
+    }
+  };
+
+  const rejectApiRequests = async (apiId) => {
+    try{
+      const response = await apiClient.get(`/admin/Api/declined/${apiId}`);
+      return response;
+
+    }catch (error){
+      throw new Error(error.response.data.error.message);
+    }
+  }
+
+
   return {
     createApiKey,
     getApiKeys,
+    getAllApiRequests,
+    acceptApiRequests,
+    rejectApiRequests
   };
 };
